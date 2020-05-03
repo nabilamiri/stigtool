dfb2a851c16aa2d9c99bbd0bffb987912c02bb5b
Nabil Amiri <nabil.amiri@gmail.com>
2020-05-03 11:47:57 -0700

    Added JSON output redirect

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ nr = InitNornir(core={'num_workers': 50}, inventory={
     }
 }, logging={ 'enabled': False })
 
-targetHosts = ['172.16.1.203', '172.16.1.204']
+targetHosts = ['172.16.1.213', '172.16.1.214']
 
 # This is taking a value from the Nornir inventory. Every host in the inventory has a variable named 'credentials' which contains either 'INTERNAL' or 'EXTERNAL'  The .env file contains the variables for both of those, named INTERNAL_NM_USER and INTERNAL_NM_PASS.
 # 'task.host.username/password' is a well-known host variable that's automatically searched for credentials by NAPALM.
@@ -63,7 +63,7 @@ def dev_switches(host):
     else:
         return False
 
-def line_by_line_comparison(existingConfiguration, linesToCompare, shouldExist, exactMatch=True):
+def line_by_line_comparison(existingConfiguration, linesToCompare, shouldExist, test='not configured', exactMatch=True):
     #run a comparison between your configDictionary and the check_content list
     #If we need to search for a term then we should pass in exactMatch=False during function call
     for key, values in existingConfiguration.items():
@@ -73,17 +73,49 @@ def line_by_line_comparison(existingConfiguration, linesToCompare, shouldExist, 
                 for current_line in v:
                     for line in linesToCompare:
                         if line == current_line and exactMatch == True:
-                            print("Issue found.", key, " contains", line, "Please remediate.")
+                            testResult = "Issue found. " + key + " contains" + line + "Please remediate."
+                            resultDictionary[h][test].append(testResult)
                         if line in current_line and exactMatch == False:
-                            print("Issue found.", key, " contains", line, "Please remediate.")
+                            testResult = "Issue found. " + key + " contains" + line + "Please remediate."
+                            resultDictionary[h][test].append(testResult)
             #Check for single instance of a line within the configurations
             if shouldExist == True:
                 for line in linesToCompare:
                         if line not in v:
-                            print("Issue found ", line, " does not exist in configuration for host", key)
+                            testResult = "Issue found " + line + " does not exist in configuration for host " + key
+                            resultDictionary[key][test].append(testResult)
+
+def checkInterface(existingConfiguration, lineToCompare, test):
+        for key, values in existingConfiguration.items():
+            for v in values:
+                print("\nCurrently checking configuration for host", key)
+                for current_line in v:
+                    #reset both counters before checking for current host
+                    hasSwitchport = False
+                    hasLineToCompare = False
+                    interface = current_line
+                    hasBoth = 0 #reset counter for hasBoth
+                    if interface.startswith("interface"):
+                        #find the index position within the configuration to check for configuration
+                        index_element = v.index(interface)
+                        while v[index_element] != '!':
+                            #for the comparison we are using a leading because the config contains a space for indentation
+                            #we have to check for routed ports and ignore those
+                            #we have to disqualify trunk ports
+                            if 'switchport mode access' in v[index_element] and v[index_element] != ' no switchport':
+                                hasSwitchport = True #increment if a match is found
+                            if lineToCompare in v[index_element]:
+                                hasLineToCompare = True
+                            if hasSwitchport == True and hasLineToCompare == True:
+                                break
+                            index_element += 1 #check the next line until reaching the end of interface configuration
+                        if hasSwitchport == True and hasLineToCompare == False:
+                            testResult = interface + "is missing " + lineToCompare +  " command. Please remediate"
+                            resultDictionary[key][test].append(testResult)
 
 def CISC_L2_000010(existingConfiguration):
     #Description: The Cisco switch must be configured to disable non-essential capabilities.
+    test = "CISC_L2_000010"
     checkContent = [
     'boot network',
     'ip boot server',
@@ -101,13 +133,13 @@ def CISC_L2_000010(existingConfiguration):
     ]
 
     shouldExist = False
-    print_title("CISC_L2_000010")
-    line_by_line_comparison(existingConfiguration, checkContent, shouldExist)
-
+    print_title(test)
+    line_by_line_comparison(existingConfiguration, checkContent, shouldExist, test)
 
 def CISC_L2_000020(existingConfiguration):
     #Description: Verify if the switch configuration has 802.1x authentication implemented for all access switch ports connecting to LAN outlets (i.e., RJ-45 wall plates) or devices not located in the telecom room, wiring closets, or equipment rooms. MAC Authentication Bypass (MAB) must be configured on those switch ports connected to devices that do not support an 802.1x supplicant.
-    print_title("CISC_L2_000020")
+    test = "CISC_L2_000020"
+    print_title(test)
     hasBoth = 0 #this will keep a count per interface to see if both critieria are met
     for key, values in existingConfiguration.items():
         for v in values:
@@ -126,7 +158,8 @@ def CISC_L2_000020(existingConfiguration):
                             break
                         index_element += 1 #check the next line until reaching the end of interface configuration
                     if hasBoth < 2:
-                        print(interface ,"is missing do1x configuration. Please remediate")
+                        testResult = str(interface) + " is missing do1x configuration. Please remediate"
+                        resultDictionary[key][test].append(testResult)
 
     #Verify 802.1x configuration on the switch
     checkContent = [
@@ -138,12 +171,12 @@ def CISC_L2_000020(existingConfiguration):
     ]
 
     shouldExist = True
-    line_by_line_comparison(existingConfiguration, checkContent, shouldExist)
-
+    line_by_line_comparison(existingConfiguration, checkContent, shouldExist, test)
 
 def CISC_L2_000030(existingConfiguration):
     #Description: The Cisco switch must authenticate all VLAN Trunk Protocol (VTP) messages with a hash function using the most secured cryptographic algorithm available.
-    print_title("CISC_L2_000030")
+    test = "CISC_L2_000030"
+    print_title(test)
     output = access_switches.run(task=networking.napalm_cli, commands=['show vtp password'])
 
     configDictionary = defaultdict(list)
@@ -152,7 +185,8 @@ def CISC_L2_000030(existingConfiguration):
         filtered_output = output[l].result
         someString = filtered_output['show vtp password']
         if 'not configured' in someString:
-            print("Host ", h,"does not have a VTP password. Please set one.")
+            testResult = "Host " +  h + " does not have a VTP password. Please set one."
+            resultDictionary[h][test].append(testResult)
         configDictionary[h].append(someString)
         filtered_output = None
         someString = None
@@ -161,115 +195,90 @@ def CISC_L2_000040(existingConfiguration):
     #Description: The Cisco switch must have STP Loop Guard enabled.
     qos = ['mls qos']
     shouldExist = True
-    print_title("CISC_L2_000040")
-    line_by_line_comparison(existingConfiguration, qos, shouldExist)
+    test = "CISC_L2_000040"
+    print_title(test)
+    line_by_line_comparison(existingConfiguration, qos, shouldExist, test)
 
 def CISC_L2_000090(existingConfiguration):
     #Description: The Cisco switch must have Root Guard enabled on all switch ports connecting to access layer switches and hosts.
-    print_title("CISC_L2_000090")
-    for key, values in existingConfiguration.items():
-        for v in values:
-            print("\nCurrently checking configuration for host", key)
-            #reset both counters before checking for current host
-            hasSwitchport = False
-            hasSpanningTreeGuardRoot = False
-
-            for current_line in v:
-                interface = current_line
-                hasBoth = 0 #reset counter for hasBoth
-                if interface.startswith("interface"):
-                    #find the index position within the configuration to check for dot1x configuration
-                    index_element = v.index(interface)
-                    while v[index_element] != '!':
-                        #for the comparison we are using a leading because the config contains a space for indentation
-                        #we have to check for routed ports and ignore those
-                        if 'switchport' in v[index_element] and v[index_element] != ' no switchport':
-                            hasSwitchport = True #increment if a match is found
-                        if 'spanning-tree guard root' in v[index_element]:
-                            hasSpanningTreeGuardRoot = True
-                        if hasSwitchport == True and hasSpanningTreeGuardRoot == True:
-                            break
-                        index_element += 1 #check the next line until reaching the end of interface configuration
-                    if hasSwitchport == True and hasSpanningTreeGuardRoot == False:
-                        print(interface ,"is missing spanning-tree guard root command. Please remediate")
+    test = "CISC_L2_000090"
+    print_title(test)
+    commandToTest = 'spanning-tree guard root'
+    checkInterface(existingConfiguration, commandToTest, test)
 
 def CISC_L2_000100(existingConfiguration):
     #Description: The Cisco switch must have BPDU Guard enabled on all user-facing or untrusted access switch ports.
-    print_title("CISC_L2_000100")
-    for key, values in existingConfiguration.items():
-        for v in values:
-            print("\nCurrently checking configuration for host", key)
-            for current_line in v:
-                #reset both counters before checking for current host
-                hasSwitchport = False
-                hasBpduGuardEnable = False
-                interface = current_line
-                hasBoth = 0 #reset counter for hasBoth
-                if interface.startswith("interface"):
-                    #find the index position within the configuration to check for configuration
-                    index_element = v.index(interface)
-                    while v[index_element] != '!':
-                        #for the comparison we are using a leading because the config contains a space for indentation
-                        #we have to check for routed ports and ignore those
-                        #we have to disqualify trunk ports
-                        if 'switchport mode access' in v[index_element] and v[index_element] != ' no switchport':
-                            hasSwitchport = True #increment if a match is found
-                        if 'spanning-tree bpduguard enable' in v[index_element]:
-                            hasBpduGuardEnable = True
-                        if hasSwitchport == True and hasBpduGuardEnable == True:
-                            break
-                        index_element += 1 #check the next line until reaching the end of interface configuration
-                    if hasSwitchport == True and hasBpduGuardEnable == False:
-                        print(interface ,"is missing bpduguard enable command. Please remediate")
+    test = "CISC_L2_000100"
+    print_title(test)
+    commandToTest = 'spanning-tree bpduguard enable'
+    checkInterface(existingConfiguration, commandToTest, test)
 
 def CISC_L2_000110(existingConfiguration):
     #Description: The Cisco switch must have STP Loop Guard enabled.
     loopGuard = ['spanning-tree loopguard default']
     shouldExist = True
-    print_title("CISC_L2_000110")
-    line_by_line_comparison(existingConfiguration, loopGuard, shouldExist)
+    test = "CISC_L2_000110"
+    print_title(test)
+    line_by_line_comparison(existingConfiguration, loopGuard, shouldExist, test)
 
 def CISC_L2_000120(existingConfiguration):
     #Description: The Cisco switch must have BPDU Guard enabled on all user-facing or untrusted access switch ports.
-    print_title("CISC_L2_000120")
-    for key, values in existingConfiguration.items():
-        for v in values:
-            print("\nCurrently checking configuration for host", key)
-            for current_line in v:
-                #reset both counters before checking for current host
-                hasSwitchport = False
-                hasSwitchportBlockUnicast = False
-                interface = current_line
-                hasBoth = 0 #reset counter for hasBoth
-                if interface.startswith("interface"):
-                    #find the index position within the configuration to check for configuration
-                    index_element = v.index(interface)
-                    while v[index_element] != '!':
-                        #for the comparison we are using a leading because the config contains a space for indentation
-                        #we have to check for routed ports and ignore those
-                        #we have to disqualify trunk ports
-                        if 'switchport mode access' in v[index_element] and v[index_element] != ' no switchport':
-                            hasSwitchport = True #increment if a match is found
-                        if 'switchport block unicast' in v[index_element]:
-                            hasBpduGuardEnable = True
-                        if hasSwitchport == True and hasSwitchportBlockUnicast == True:
-                            break
-                        index_element += 1 #check the next line until reaching the end of interface configuration
-                    if hasSwitchport == True and hasSwitchportBlockUnicast == False:
-                        print(interface ,"is missing switchport block unicast command. Please remediate")
+    test = "CISC_L2_000120"
+    print_title(test)
+    commandToTest = 'switchport block unicast'
+    checkInterface(existingConfiguration, commandToTest, test)
 
+def CISC_L2_000140(existingConfiguration):
+    test = "CISC_L2_000140"
+    print_title(test)
+    commandToTest = 'ip verify source'
+    checkInterface(existingConfiguration, commandToTest, test)
 
+def CISC_L2_000160(existingConfiguration):
+    test = "CISC_L2_000160"
+    print_title(test)
+    commandToTest = 'storm-control unicast'
+    checkInterface(existingConfiguration, commandToTest, test)
+    commandToTest = 'storm-control broadcast'
+    checkInterface(existingConfiguration, commandToTest, test)
 
-def CISC_L2_000270(existingConfiguration):
-    print_title("CISC_L2_000270")
-
-    checkContent = [
-    'native vlan'
-    ]
-
+def CISC_L2_000170(existingConfiguration):
+    #Description: The Cisco switch must have IGMP or MLD Snooping configured on all VLANs.
+    test = "CISC_L2_000170"
+    print_title(test)
+    commandToTest = ['no ip igmp snooping']
     shouldExist = False
     exactMatch = False
-    line_by_line_comparison(existingConfiguration, checkContent, shouldExist, exactMatch)
+    line_by_line_comparison(existingConfiguration, commandToTest, shouldExist, test, exactMatch)
+
+def CISC_L2_000180(existingConfiguration):
+    #Description: The Cisco switch must implement Rapid STP where VLANs span multiple switches with redundant links.
+    test = "CISC_L2_000180"
+    print_title(test)
+    commandToTest = ['spanning-tree mode rapid-pvst']
+    shouldExist = True
+    line_by_line_comparison(existingConfiguration, commandToTest, shouldExist, test)
+
+def CISC_L2_000190(existingConfiguration):
+    #Description: The Cisco switch must enable Unidirectional Link Detection (UDLD) to protect against one-way connections.
+    test = "CISC_L2_000190"
+    print_title(test)
+    commandToTest = ['udld enable']
+    shouldExist = True
+    line_by_line_comparison(existingConfiguration, commandToTest, shouldExist, test)
+
+    #This will check on a per interface basis. We should consider modifying line by line to a boolean return...
+    commandToTest = 'udld port'
+    checkInterface(existingConfiguration, commandToTest, test)
+
+def CISC_L2_000270(existingConfiguration):
+    #Description: The Cisco switch must not have any switchports assigned to the native VLAN.
+    test = "CISC_L2_000270"
+    print_title(test)
+    checkContent = ['native vlan']
+    shouldExist = False
+    exactMatch = False
+    line_by_line_comparison(existingConfiguration, checkContent, shouldExist, test, exactMatch)
 
 #def push_acl_config(task):
 #    result = task.run(task=networking.napalm_configure, name='Loading config', configuration=task.host['config'], dry_run=False)
@@ -281,15 +290,6 @@ access_switches = nr.filter(filter_func=dev_switches)
 # Run the task (python function) named populate_credentials on those devices
 print_title('Populate credentials')
 access_switches.run(task=populate_credentials)
-
-# Generate a config snippet per device and save it in a host variable... In this case the config is the same for every device
-#print_title('Generate ACL configuration for access switches')
-#access_switches.run(task=generate_config)
-
-# Actually send the config to the device. For IOS, NAPALM uses SCP and loads it in an atomic operation
-#print_title('Deploy ACL configuration to access switches')
-#result = access_switches.run(task=push_acl_config)
-
 output = access_switches.run(task=collect_configurations)
 
 #generate a list of all hosts
@@ -316,6 +316,11 @@ for h, l in zip(hostKeys, listOfKeys):
 #        for i in v:
 #            print(i)
 
+#Generate data structures (a dictionary of dictionary of lists) for the results to be captured
+resultDictionary = defaultdict(dict)
+for h in hostKeys:
+    resultDictionary[h] = defaultdict(list)
+
 CISC_L2_000010(configDictionary)
 CISC_L2_000020(configDictionary)
 CISC_L2_000030(configDictionary)
@@ -324,4 +329,21 @@ CISC_L2_000090(configDictionary)
 CISC_L2_000100(configDictionary)
 CISC_L2_000110(configDictionary)
 CISC_L2_000120(configDictionary)
+#CISC_L2_000130 - will need to build logic around ranges
+CISC_L2_000140(configDictionary)
+#CISC_L2_000150 - similar code to 130
+CISC_L2_000160(configDictionary)
+CISC_L2_000170(configDictionary)
+CISC_L2_000180(configDictionary)
+CISC_L2_000190(configDictionary)
+#CISC-L2-000240 - No usage of VLAN1
+#CISC-L2-000250 - Need clarification on this requirement
+#CISC_L2_000260 - inherently satisfied by 270
 CISC_L2_000270(configDictionary)
+
+# Serializing json
+json_object = json.dumps(resultDictionary, indent = 4)
+
+# Writing to result.json
+with open("results.json", "w") as outfile:
+    outfile.write(json_object)
